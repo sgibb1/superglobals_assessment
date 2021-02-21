9a2d2e344aaad1961dc0a1b1cc5040724b083a20
sgibb1 <35509885+sgibb1@users.noreply.github.com>
2021-02-20 21:41:51 -0500

    added img and made chages to some of the code
    Ready to submit

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 
 <body>
 
-<header><?php echo htmlspecialchars($_GET['title']); ?></header>
+<header><h1><?php echo htmlspecialchars($_GET['title']); ?></h1></header>
 
 <h2>Year Released: <?php echo htmlspecialchars($_GET['year']); ?></h2>
 
 <h2>Gross Revenue: <?php echo htmlspecialchars($_GET['box']); ?> Million</h2>
 
-<h2>This is the movie poster: <?php echo htmlspecialchars($_GET['poster']); ?></h2>
+<h2>This is the movie poster:</h2> <img src="<?php echo htmlspecialchars($_GET['poster']);?>"></img>
   
-<!-- https://img.moviepostershop.com/rounders-movie-poster-1998-1010349809.jpg -->
-
+<!-- title: Rounders year: 1998 revenue: 22 poster: https://pnimg.net/w/articles-attachments/1/5b9/7eba4e3879.jpg -->
+  
 </body>
 </html>
