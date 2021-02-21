7641c3a82035c40a146ef96ae9f13b6da2e2f19e
sgibb1 <35509885+sgibb1@users.noreply.github.com>
2021-02-20 21:07:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 <h2>Gross Revenue: <?php echo htmlspecialchars($_GET['box']); ?> Million</h2>
 
 <h2>This is the movie poster: <?php echo htmlspecialchars($_GET['poster']); ?></h2>
+  
+<!-- https://img.moviepostershop.com/rounders-movie-poster-1998-1010349809.jpg -->
 
 </body>
 </html>
